fe3b3da7fa62765c60f96b68608456e82d3a5a24
anshu pande <anshu@adobe.com>
2016-06-23 13:59:14 -0400

    remove typo

--- v1/util/etcd2-setup.sh
+++ v1/util/etcd2-setup.sh
@@ -27,8 +27,7 @@ systemctl start etcd-peers
 
 echo "-------Waiting for etcd2 to start-------"
 
-  sudo -E docker run \
-while [[ $(etcdctl -u $ROOT_USERNAME:$ROOT_PASSWORD cluster-health|grep unhealthy) || $(etcdctl -u $ROOT_USERNAME:$ROOT_PASSWORD member list | wc -l) -lt $CONTROL_CLUSTER_SIZE ]]
+while [[ $(etcdctl cluster-health|grep unhealthy) || $(etcdctl member list | wc -l) -lt $CONTROL_CLUSTER_SIZE ]]
 do
   sleep 8
 done
