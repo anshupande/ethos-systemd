f8f37ddf0540ae74cf9e496efc5bd51b76d84b34
anshu pande <anshu@adobe.com>
2016-06-24 11:14:23 -0400

    replace etc-get with etc-authget function

--- v1/opt/ecr/setup/leader/001-images.sh
+++ v1/opt/ecr/setup/leader/001-images.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
-etcd-set /images/ecr-login "index.docker.io/behance/ecr-login:latest"
+etcd-authset /images/ecr-login "index.docker.io/behance/ecr-login:latest"
--- v1/opt/sumologic/setup/leader/001-images.sh
+++ v1/opt/sumologic/setup/leader/001-images.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
-etcd-set /images/sumologic              "index.docker.io/behance/docker-sumologic:latest"
-etcd-set /images/sumologic-syslog       "index.docker.io/behance/docker-sumologic:syslog-latest"
+etcd-authset /images/sumologic              "index.docker.io/behance/docker-sumologic:latest"
+etcd-authset /images/sumologic-syslog       "index.docker.io/behance/docker-sumologic:syslog-latest"
