1445732da4facd983e75275e38fed457102aacc4
anshu pande <anshu@adobe.com>
2016-09-13 17:31:40 -0400

    move logic to check etcd value to fleet

--- v1/util/selinux.sh
+++ v1/util/selinux.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash -x
 
-# Check if Sumologic is installed as optional service, if yes then exit and do not enable selinux
-
-if [[ $(etcdctl get /environment/services |grep sumologic) = *sumologic* ]]; then   exit 0; fi
-
-
 #To enable SELinux enforcement across reboots, replace the symbolic link /etc/selinux/config with the file it targets, so that the file can be written. You can use the readlink command to dereference the link, as shown in the following one-liner:
 
 sudo cp --remove-destination $(readlink -f /etc/selinux/config) /etc/selinux/config
