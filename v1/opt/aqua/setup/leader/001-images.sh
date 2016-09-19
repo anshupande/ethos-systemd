75207cc5ec54c2c40924da2255c45156fffe12ec
anshu pande <anshu@adobe.com>
2016-09-19 11:30:43 -0400

    add aqua-launcher image based on docker version

--- v1/opt/aqua/setup/leader/001-images.sh
+++ v1/opt/aqua/setup/leader/001-images.sh
@@ -6,3 +6,14 @@ source $DIR/../../../../lib/helpers.sh
 etcd-set /images/scalock-gateway    "index.docker.io/aquasec/gateway:1.2"
 etcd-set /images/scalock-agent      "index.docker.io/aquasec/agent:1.2.1"
 etcd-set /images/scalock-server     "index.docker.io/aquasec/server:1.2"
+
+
+AQUA_INSTALLED=$(docker version | grep -i 1.10.3)
+
+if [[ "$AQUA_INSTALLED" == ""  ]];
+then
+etcd-set /images/aqua-launcher               \"index.docker.io/behance/aqua-launcher:1.9.1\";
+else
+etcd-set /images/aqua-launcher               \"index.docker.io/behance/aqua-launcher:latest\";
+fi
+
